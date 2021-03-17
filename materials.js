3bdcd243d61d67ded82bac65c2fc87f2244125f3
ArtemRostomyan <71639600+ArtemRostomyan@users.noreply.github.com>
2021-03-17 17:39:33 +0400

    Add files via upload

---
+++ materials.js
@@ -0,0 +1,20 @@
+[
+    {
+        "id": "mat-1",
+        "name": "шенил",
+        "price": 1000,
+        "available": true
+    },
+    {
+        "id": "mat-2",
+        "name": "велюр",
+        "price": 2000,
+        "available": false
+    },
+    {
+        "id": "mat-2",
+        "name": "рогожка",
+        "price": 2800,
+        "available": true
+    }
+]
