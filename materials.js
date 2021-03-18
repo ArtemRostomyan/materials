bf6d6ef77acaff2478edffbda52e7327eb32d5bd
ArtemRostomyan <71639600+ArtemRostomyan@users.noreply.github.com>
2021-03-18 16:42:11 +0400

    Update materials.js

--- materials.js
+++ materials.js
@@ -3,18 +3,21 @@
         "id": "mat-1",
         "name": "шенил",
         "price": 1000,
-        "available": true
+        "available": true,
+        "complexity_organization": 3.5
     },
     {
         "id": "mat-2",
         "name": "велюр",
         "price": 2000,
-        "available": false
+        "available": false,
+        "complexity_organization": 4.5
     },
     {
         "id": "mat-2",
         "name": "рогожка",
-        "price": 2800,
-        "available": true
+        "price": 3000,
+        "available": true,
+        "complexity_organization": 5.5
     }
 ]
