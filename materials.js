68504b4a65ee96d07f2cf70a6f94d89f03ba66bb
ArtemRostomyan <71639600+ArtemRostomyan@users.noreply.github.com>
2021-05-05 18:04:32 +0400

    Update materials.js

--- materials.js
+++ materials.js
@@ -3,6 +3,7 @@
         "id": "mat-1",
         "type": "ткань",
         "name": "шенил",
+        "image": "https://sofme.ru/images/0036/3388.jpg",
         "price": 1000,
         "available": true,
         "complexity_organization": 3.5
@@ -11,16 +12,54 @@
         "id": "mat-2",
         "type": "ткань",
         "name": "велюр",
+        "image": "https://images.ru.prom.st/752681796_w640_h640_marvel-greener-pastures.jpg",
         "price": 2000,
         "available": false,
         "complexity_organization": 4.5
     },
     {
-        "id": "mat-2",
+        "id": "mat-3",
         "type": "ткань",
         "name": "рогожка",
+        "image": "https://ametist-store.ru/upload/iblock/40b/TWIST_terracotta.jpg",
         "price": 2800,
         "available": true,
         "complexity_organization": 5.5
+    },
+    {
+        "id": "mat-4",
+        "type": "кожа",
+        "name": "кожа италия",
+        "image": "https://mtf-sib.ru/wp-content/uploads/2020/08/ranch.jpg",
+        "price": 5800,
+        "available": true,
+        "complexity_organization": 6.5
+    },
+    {
+        "id": "mat-5",
+        "type": "кожа",
+        "name": "кожа италия (зелёная)",
+        "image": "https://briskmebel.ru/d/grass.jpg",
+        "price": 5000,
+        "available": false,
+        "complexity_organization": 5.8
+    },
+    {
+        "id": "mat-6",
+        "type": "ткань",
+        "name": "велюр (красный)",
+        "image": "https://wsemoboi.ru/image/catalog/tkani/katalog-tkaney/designers-guild/zaragoza/190623_angliyskaya-tkan-designers-guild-kollektsiya-zaragoza-artikul-fdg2333-54.jpg",
+        "price": 2100,
+        "available": false,
+        "complexity_organization": 2.1
+    },
+    {
+        "id": "mat-6",
+        "type": "ткань",
+        "name": "рогожка (жёлтая)",
+        "image": "https://sofme.ru/images/0029/3424.jpg",
+        "price": 2500,
+        "available": true,
+        "complexity_organization": 1.6
     }
 ]
